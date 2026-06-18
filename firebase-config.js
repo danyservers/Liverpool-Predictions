e1d3973d14e91201b30604607f507f4af8624b6f
danyservers <danielhermansen94@hotmail.com>
2026-06-18 13:45:18 +0200

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,25 @@
+// Firebase config for your project.
+// IMPORTANT: Change both emails below to the exact Firebase Authentication emails
+// you create for Dany and Isa.
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyA1lXwSOmMBWWoyhBHRjuJ0bQTaO7385Zc",
+  authDomain: "liverpool-prediction.firebaseapp.com",
+  projectId: "liverpool-prediction",
+  storageBucket: "liverpool-prediction.firebasestorage.app",
+  messagingSenderId: "751064521572",
+  appId: "1:751064521572:web:0e432c7a9bcbf3fb297871"
+};
+
+export const USER_PROFILES = {
+  "babe1liverpool@hotmail.com": {
+    key: "dany",
+    name: "Dany"
+  },
+  "babe2liverpool@hotmail.com": {
+    key: "isa",
+    name: "Isa"
+  }
+};
+
+export const DEFAULT_SEASON_ID = "2025-26";
